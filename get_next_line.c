667db250030f10779514a76d10f7f0a7654df219
gerardenrico <81775867+gerardenrico@users.noreply.github.com>
2022-01-29 12:54:54 +0400

    upload get_next_line project

---
+++ get_next_line.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 10:05:53 by gkintana          #+#    #+#             */
+/*   Updated: 2022/01/29 12:50:56 by gkintana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static void	ft_free(char **stat_buff, char *set_to)
+{
+	free(*stat_buff);
+	*stat_buff = set_to;
+}
+
+static char	*one_line(char **stat_buff)
+{
+	char	*line;
+	char	*temp;
+	int		i;
+
+	i = 0;
+	while (stat_buff[PTR][i] != 0 && stat_buff[PTR][i] != 10)
+		i++;
+	if (!stat_buff[PTR][i])
+	{
+		line = ft_strdup(&stat_buff[PTR][0]);
+		ft_free(stat_buff, NULL);
+	}
+	else
+	{
+		line = ft_substr(&stat_buff[PTR][0], 0, i + 1);
+		temp = ft_strdup(&stat_buff[PTR][i + 1]);
+		ft_free(stat_buff, temp);
+		if (!stat_buff[PTR][0])
+			ft_free(stat_buff, NULL);
+	}
+	return (line);
+}
+
+static void	read_fd(int fd, char **stat_buff, char *buffer, ssize_t bytes)
+{
+	char	*store;
+
+	store = NULL;
+	while (bytes > 0)
+	{
+		buffer[bytes] = 0;
+		if (!stat_buff[PTR])
+			stat_buff[PTR] = ft_strdup("");
+		store = ft_strjoin(&stat_buff[PTR][0], buffer);
+		ft_free(&stat_buff[PTR], store);
+		if (ft_strchr(stat_buff[PTR], 10))
+			break ;
+		bytes = read(fd, buffer, BUFFER_SIZE);
+	}
+	free(buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stat_buff;
+	char		*buffer;
+	ssize_t		bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	read_fd(fd, &stat_buff, buffer, bytes);
+	if (!stat_buff && bytes <= 0)
+		return (NULL);
+	else
+		return (one_line(&stat_buff));
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 10:05:53 by gkintana          #+#    #+#             */
+/*   Updated: 2022/01/29 12:50:53 by gkintana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
+
+	str = (char *)s;
+	if (c == '\0')
+		return (str + ft_strlen(str));
+	i = -1;
+	while (str[++i] != '\0')
+		if (str[i] == (char)c)
+			return (str + i);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*join;
+
+	if (!s1 || !s2)
+		return (NULL);
+	join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (NULL);
+	i = -1;
+	while (s1[++i] != '\0')
+		join[i] = s1[i];
+	j = -1;
+	while (s2[++j] != '\0')
+		join[i + j] = s2[j];
+	join[i + j] = '\0';
+	return (join);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*dup;
+
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!dup)
+		return (NULL);
+	i = -1;
+	while (s1[++i] != '\0')
+		dup[i] = s1[i];
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
+	if (start >= ft_strlen(s))
+		return ((ft_strdup("")));
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len)
+		sub[i++] = s[start++];
+	sub[i] = '\0';
+	return (sub);
+}
